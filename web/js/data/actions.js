88ad38ffc1786d1ff0bbe5c40296b2bbd971dc3c
William <williamzhuk@gmail.com>
2016-07-12 20:26:30 -0700

    thunky

--- web/js/data/actions.js
+++ web/js/data/actions.js
@@ -16,3 +16,23 @@ export function setVisibilityFilter(filter){
         filter: filter
     };
 }
+// this requires redux-thunk only
+export function promiseThunk(param) {
+    return (dispatch) => {
+        dispatch({type: 'PT_REQUEST', param: param});
+        return fetch(`http://foo/${param}`)
+            .then(r => r.json())
+            .then(r => {
+                dispatch({type: 'PT_SUCCESS', param: param, response: r});
+                return r;
+            })
+            .catch(e => {
+                dispatch({type: 'PT_ERROR', param: param, error: e});
+                throw e;
+            })
+    };
+}
+// this requires redux-promise-middleware
+export function promiseAction(param) {
+    return fetch(`http://foo/${param}`).then(r => r.json());
+}
