f5f8c4019027eff55024ab6f15f3424591e7d0dd
William <williamzhuk@gmail.com>
2016-07-21 19:07:36 -0700

    Fake API localStorage

--- web/js/data/api.js
+++ web/js/data/api.js
@@ -1,24 +1,40 @@
 import {v4} from 'node-uuid';
 
-let fakeStore = {
-    '/api/todo': {
-        "45c54404-91ba-4e43-bb38-d60df0b6f645": {
-            "id": "45c54404-91ba-4e43-bb38-d60df0b6f645",
-            "text": "sadf",
-            "completed": false
-        },
-        "a28b6068-eb50-4ff2-bf9e-5d0cd185d890": {
-            "id": "a28b6068-eb50-4ff2-bf9e-5d0cd185d890",
-            "text": "asdfa",
-            "completed": false
-        },
-        "4fb163e4-b4df-4073-b891-8e61f4161e6a": {
-            "id": "4fb163e4-b4df-4073-b891-8e61f4161e6a",
-            "text": "asdfsadf",
-            "completed": true
-        }
+// let fakeStore = {
+//     '/api/todo': {
+//         "45c54404-91ba-4e43-bb38-d60df0b6f645": {
+//             "id": "45c54404-91ba-4e43-bb38-d60df0b6f645",
+//             "text": "sadf",
+//             "completed": false
+//         },
+//         "a28b6068-eb50-4ff2-bf9e-5d0cd185d890": {
+//             "id": "a28b6068-eb50-4ff2-bf9e-5d0cd185d890",
+//             "text": "asdfa",
+//             "completed": false
+//         },
+//         "4fb163e4-b4df-4073-b891-8e61f4161e6a": {
+//             "id": "4fb163e4-b4df-4073-b891-8e61f4161e6a",
+//             "text": "asdfsadf",
+//             "completed": true
+//         }
+//     }
+// };
+
+let fakeStoreInit = {'/api/todo': {}};
+
+function getFakeStore() {
+    try {
+        return localStorage['fakeStore']
+            ? JSON.parse(localStorage['fakeStore'])
+            : fakeStoreInit;
+    } catch (e) {
+        return fakeStoreInit;
     }
-};
+}
+
+function setFakeStore(fakeStore) {
+    localStorage['fakeStore'] = JSON.stringify(fakeStore);
+}
 
 export function save(prefix) {
     return (body) => {
@@ -31,21 +47,24 @@ export function save(prefix) {
         body.text = body.text || '';
         let url = isNew ? '' : `/${body.id}`;
         //return fetch(`${prefix}${url}`, {body: body, method: isNew ? 'POST' : 'PUT'})
+        let fakeStore = getFakeStore();
         fakeStore[prefix][body.id] = body;
-        return new Promise((resolve, reject)=>{
-            setTimeout(()=>{
+        setFakeStore(fakeStore);
+        return new Promise((resolve, reject)=> {
+            setTimeout(()=> {
                 resolve(body);
-            },500);
+            }, 200);
         })
     }
 }
 
 export function list(prefix) {
+    let fakeStore = getFakeStore();
     let array = Object.keys(fakeStore[prefix]).map(index => fakeStore[prefix][index]);
     //return Promise.resolve(array);
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=> {
             resolve(array);
-        },1000);
+        }, 500);
     });
 }
